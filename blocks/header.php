0770faf84ecd310c8f80fae282c5b911175e3f64
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-05-03 21:29:41 +0300

    Create header.php

---
+++ blocks/header.php
@@ -0,0 +1,6 @@
+<header>
+<h4><?php echo "wellcome to the club ".$_COOKIE['user_name'] ?> || <a href="logout.php">log out of the account</a><br></h4>
+    <h4>| <a href="home.php">home page</a> |
+    <a href="index.php">register page</a> |
+    <a href="discussion.php">discussion page</a> |</h4>
+</header>
