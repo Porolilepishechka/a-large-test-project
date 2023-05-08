e3ea25afebf8a69f4cbe256bf06caaccf0117389
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-05-08 18:48:57 +0300

    Update post-manager.php

--- blocks/post-manager.php
+++ blocks/post-manager.php
@@ -1,13 +1,12 @@
 <?php 
-
 if($_COOKIE['user_name'] == ""){
     echo "ви не авторизовані на сайті";
     echo '<br><a href="../index.php">register page</a>';
 exit();
-}
-
-if($_COOKIE['user_name'] != "")
+} // перевіряє чи авторизований користувач
+else if($_COOKIE['user_name'] != "")
     $user_name = $_COOKIE['user_name'];
+// запихує імя користувача в змінну
 
 $theme = $_POST['theme'];
 $message = $_POST['massage'];
@@ -21,13 +20,13 @@ exit();
     echo '<br><a href="../home.php">home page</a>';
 exit();
 }
+// куча перевірок поста
 
 $mysql = new mysqli("localhost", "root", "", "Test db");
 $mysql->query("SET NAMES 'utf8'");
-
 $mysql->query("INSERT INTO `posts` (`user_name`, `theme`, `message`) VALUES('$user_name','$theme', '$message')");
-
+// підключається до бд та зберігає пост в ній
 $mysql->close();
 
-header('Location: ../index.php');
+header('Location: ../discussion.php');
 ?>
